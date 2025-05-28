a33a47dbcf7f0810a04a6439d32e9bf9060c7147
LenzAlanoca <laalanoca3@umsa.bo>
2025-05-28 11:51:26 -0400

    correcciones

---
+++ src/main/java/com/sgeumsaapi/sge_umsa_api/model/Rol.java
@@ -0,0 +1,9 @@
+// src/main/java/com/sgeumsaapi/sge_umsa_api/model/Rol.java
+
+package com.sgeumsaapi.sge_umsa_api.model;
+
+public enum Rol {
+    CLIENTE,
+    ADMIN,
+    ORGANIZADOR
+}
--- src/main/java/com/sgeumsaapi/sge_umsa_api/model/Usuario.java
+++ src/main/java/com/sgeumsaapi/sge_umsa_api/model/Usuario.java
@@ -16,53 +16,51 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-//generacion tabla
+
 @Entity
 @Table(name = "usuario")
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Usuario {
-    
+
     @Id
     @Column(nullable = false, unique = true)
     private Long ci;
-    
+
     @Version
     private Long version;
-    
-    @Column(nullable = false,length = 50)
+
+    @Column(nullable = false, length = 50)
     @Basic(optional = false)
-    @NotBlank(message = "El nombre no puede estar vacio")
-    @Length(min = 3 , max = 50, message = "El nombre debe tener entre 3 y 50 caracteres")
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @Length(min = 3, max = 50, message = "El nombre debe tener entre 3 y 50 caracteres")
     private String nombre;
-    
-    @Column(nullable = false,length = 50)
+
+    @Column(nullable = false, length = 50)
     @Basic(optional = false)
-    @NotBlank(message = "El apellido no puede estar vacio")
-    @Length(min = 3 , max = 50, message = "El apellido debe tener entre 3 y 50 caracteres")
+    @NotBlank(message = "El apellido no puede estar vacío")
+    @Length(min = 3, max = 50, message = "El apellido debe tener entre 3 y 50 caracteres")
     private String apellido;
-    
-    @Column(nullable = false,unique = true)
+
+    @Column(nullable = false, unique = true)
     @Basic(optional = false)
-    @NotBlank(message = "El correo no puede estar vacio")
+    @NotBlank(message = "El correo no puede estar vacío")
     @Email
     private String correo;
-    
-    @NotBlank(message = "El usuario no puede estar vacio")
-    @Column(nullable = false,length= 20)
-    @Length(min = 3 , max = 20, message = "El nombre debe tener entre 3 y 20 caracteres")
+
+    @NotBlank(message = "El usuario no puede estar vacío")
+    @Column(nullable = false, length = 20)
+    @Length(min = 3, max = 20, message = "El usuario debe tener entre 3 y 20 caracteres")
     private String usuario;
 
-    @Column(nullable = false,length= 20)
     @Enumerated(EnumType.STRING)
-    @NotBlank(message = "El rol no puede estar vacio")
-    private String rol;
-    
-    @Column(nullable = false,length= 15)
+    @Column(nullable = false, length = 20)
+    private Rol rol;
+
+    @Column(nullable = false, length = 15)
     @Size(min = 7, max = 15, message = "El teléfono debe tener entre 7 y 15 caracteres")
     private String telefono;
-    
+
     private String foto;
 }
