fff75e2365089d8fa36ee1a9818ca2c89f2d076a
Cesar Ticona <cesarju.cc@gmail.com>
2025-06-11 01:09:13 -0400

    update test UserRepositoryTest

--- src/test/java/com/sgeumsaapi/sge_umsa_api/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/sgeumsaapi/sge_umsa_api/repository/UsuarioRepositoryTest.java
@@ -1,10 +1,11 @@
 package com.sgeumsaapi.sge_umsa_api.repository;
 
-
 import com.sgeumsaapi.sge_umsa_api.model.Usuario;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,36 +15,29 @@ class UsuarioRepositoryIT {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private TestEntityManager entityManager;
 
-    @Test
-    void deberiaEncontrarUsuarioPorNombreUsuario() {
-        // Given
-        Usuario usuario = new Usuario();
-        usuario.setCi("1234567");
-        usuario.setNombre("María");
-        usuario.setApellido("Gómez");
-        usuario.setCorreo("maria.gomez@umsa.edu");
-        usuario.setRol("ESTUDIANTE");
-        usuario.setUsuario("mgomez");
-
-        usuarioRepository.save(usuario);
-
-        boolean existe = usuarioRepository.existsByUsuario("mgomez");
-        assertThat(existe).isTrue();
+    @BeforeEach
+    void setUp() {
+        usuarioRepository.deleteAll();
     }
 
     @Test
     void noDeberiaEncontrarUsuarioPorCorreoInexistente() {
+        // When
         boolean existe = usuarioRepository.existsByCorreo("correo.inexistente@umsa.edu");
+
+        // Then
         assertThat(existe).isFalse();
     }
 
     @Test
     void noDeberiaEncontrarUsuarioPorNombreUsuarioInexistente() {
+        // When
         boolean existe = usuarioRepository.existsByUsuario("usuario_inexistente");
 
+        // Then
         assertThat(existe).isFalse();
     }
-
-       
 }
